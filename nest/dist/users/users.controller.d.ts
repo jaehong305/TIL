405cf66cab74939188918f62ee9b84568babaec8
jaehong305 <jaehong30577@gmail.com>
2022-05-16 21:05:29 +0900

    22.5.16 nest

---
+++ nest/dist/users/users.controller.d.ts
@@ -0,0 +1,9 @@
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+export declare class UsersController {
+    createUser(dto: CreateUserDto): Promise<void>;
+    verifyEmail(dto: VerifyEmailDto): Promise<string>;
+    login(dto: UserLoginDto): Promise<string>;
+    getUserInfo(userId: string): Promise<string>;
+}
